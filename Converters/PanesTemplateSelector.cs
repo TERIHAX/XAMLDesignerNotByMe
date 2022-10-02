28d0e409efa697b4ce2f19e124450af3c6c1c5c8
TERIZOROGAME <69073505+TERIHAX@users.noreply.github.com>
2022-10-02 19:04:22 +0100

    Add files via upload

---
+++ Converters/PanesTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using AvalonDock.Layout;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ICSharpCode.XamlDesigner.Converters
+{
+	class PanesTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate DocumentTemplate
+		{
+			get;
+			set;
+		}
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			var itemAsLayoutContent = item as LayoutContent;
+
+			if (item is Document)
+				return DocumentTemplate;
+
+			return base.SelectTemplate(item, container);
+		}
+	}
+}
